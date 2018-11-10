36b9127f9e9c3bcf09fdc03e3e7e96a9dfe9e922
Andreea Dima <dimaandreea96@yahoo.com>
2018-11-10 06:11:26 +0200

    Added delay, pin, done, cancel #DoMore

--- Android/JuliasHouse/app/src/main/java/com/example/pemil/juliashouse/SitActivity.java
+++ Android/JuliasHouse/app/src/main/java/com/example/pemil/juliashouse/SitActivity.java
@@ -2,12 +2,23 @@ package com.example.pemil.juliashouse;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SitActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sit_layout);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.delay_values, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
 
