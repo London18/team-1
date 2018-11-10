a1e844e3017276cca085382f6d593d4b42ebe387
Adrian Grajdeanu <grajdeanuadrian@gmail.com>
2018-11-10 02:21:05 +0000

    Done routing

--- WebFrontend/app.py
+++ WebFrontend/app.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    return redirect('/sit')
+
+@app.route('/sit')
+def sits():
     return render_template('index.html')
 
 @app.route('/sit/add')
 def sit_add():
     return render_template('sit-form.html')
 
+@app.route('/sit/modify')
+def sit_modify():
+    return render_template('sit-form.html')
+
+@app.route('/carer')
+def carers():
+    return render_template('index.html');
+
 @app.route('/carer/add')
 def carer_add():
-    return render_template('carer-form.html')
+    return render_template('carer-form.html');
+
+@app.route('/carer/modify')
+def carer_modify():
+    return render_template('carer-form.html');
 
 @app.route('/404')
 def error404():
