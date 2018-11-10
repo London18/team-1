19a29988984e9bfd79cc900378732902674aa0ca
Adrian Grajdeanu <grajdeanuadrian@gmail.com>
2018-11-10 11:25:27 +0000

    add new carer form done

--- WebFrontend/app.py
+++ WebFrontend/app.py
@@ -11,24 +11,62 @@ app = Flask(__name__)
 def hello_world():
     return redirect('/sit')
 
+
 @app.route('/sit')
 def sits():
     data = requests.get('https://code-for-good.herokuapp.com/api/sit/getAll', json={}).json()
     return render_template('index.html', data=data, parse=dateparser.parse)
 
-@app.route('/sit/add')
+
+@app.route('/sit/add', methods=['GET', 'POST'])
 def sit_add():
-    return render_template('sit-form.html')
+    if request.method == 'POST':
+        patientId = request.form['patientId']
+        date = request.form['dateinput']
+        startdate = request.form['starttimeinput']
+        enddate = request.form['endtimeinput']
+        carer = list()
+        carer.append(int(request.form['carer_1_input']))
+        carer.append(int(request.form['carer_2_input']))
+        carer.append(int(request.form['carer_3_input']))
+        carer.append(int(request.form['carer_4_input']))
+        carer.append(int(request.form['carer_5_input']))
+        carer.append(int(request.form['carer_6_input']))
+
+        startdate = datetime.strptime(date+startdate, '%Y-%m-%d%I:%M')
+        enddate = datetime.strptime(date+enddate, '%Y-%m-%d%I:%M')
+        data = {}
+        data['startDate'] = startdate.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        data['endDate'] = enddate.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        data['patientId'] = patientId
+        data['canceled'] = False
+        data['carrersIds'] = [element for element in carer if element != 0]
+
+        json_data = json.dumps(data)
+        print(json_data)
+        headers = {
+            'Content-Type': "application/json",
+        }
+        response = requests.request("POST", "https://code-for-good.herokuapp.com/api/sit/add-sit",
+                                    data=json_data, headers=headers)
+
+        return redirect('/sit')
+    else:
+        carers = requests.get('https://code-for-good.herokuapp.com/api/user/getAll', json={}).json()
+        return render_template('sit-form.html', carers=carers)
+
 
 @app.route('/sit/modify')
 def sit_modify():
     return render_template('sit-form.html')
+
 
 @app.route('/carer')
 def carers():
     data = requests.get('https://code-for-good.herokuapp.com/api/user/getAll', json={}).json()
     print(data)
     return render_template('carers-list.html', data=data)
+
 
 @app.route('/carer/add', methods=['GET', 'POST'])
 def carer_add():
@@ -51,9 +89,18 @@ def carer_add():
     else:
         return render_template('carer-form.html')
 
+
+@app.route('/carer/alert')
+def carers_missing():
+    data = requests.get('https://code-for-good.herokuapp.com/api/user/get-all-not-home-safe', json={}).json()
+    print(data)
+    return render_template('carers-list.html', data=data)
+
+
 @app.route('/carer/modify')
 def carer_modify():
     return render_template('carer-form.html')
+
 
 @app.route('/404')
 def error404():
