0b9c43740c91e8fbf695422a712850dc89247cf4
Andreea Dima <dimaandreea96@yahoo.com>
2018-11-10 09:12:24 +0200

    Layout moficiations

--- Android/JuliasHouse/app/src/main/java/com/example/pemil/juliashouse/SitActivity.java
+++ Android/JuliasHouse/app/src/main/java/com/example/pemil/juliashouse/SitActivity.java
@@ -1,11 +1,15 @@
 package com.example.pemil.juliashouse;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SitActivity extends AppCompatActivity {
+    int duration = Toast.LENGTH_SHORT;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,28 @@ public class SitActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+    }
+
+    public void cancelSit(View view) {
+        Toast.makeText(getApplicationContext(), R.string.sit_cancelled_message, duration).show();
+    }
+
+    public void sitDone(View view) {
+        Toast.makeText(getApplicationContext(), R.string.sit_done_message, duration).show();
+    }
+
+
+    public void arrivedAtDestination(View view) {
+        Toast.makeText(getApplicationContext(), R.string.arrived_message, duration).show();
+    }
+
+    public void addComment(View view) {
+        findViewById(R.id.button_save).setVisibility(View.VISIBLE);
+    }
+
+    public void saveComment(View view) {
+        findViewById(R.id.button_save).setVisibility(View.GONE);
+        Toast.makeText(getApplicationContext(), R.string.comment_saved_message, duration).show();
     }
 }
 
