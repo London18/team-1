d0b3ba0f201d0a91056b6be6cdb1a2a754cb7e7b
pemil <p.emilia13@yahoo.com>
2018-11-10 04:31:09 +0200

    added service intent for a notification (not finished)

---
+++ Android/JuliasHouse/app/src/main/java/com/example/pemil/juliashouse/notifications/NotificationIntentService.java
@@ -0,0 +1,38 @@
+package com.example.pemil.juliashouse.notifications;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.example.pemil.juliashouse.R;
+
+public class NotificationIntentService extends IntentService {
+    public NotificationIntentService() {
+        super("NotificationIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        //TODO - modify EmptyActivity to SitActivity when created
+        Intent newIntent = new Intent(NotificationIntentService.this, EmptyActivity.class);
+        newIntent.putExtra("test", "test");
+        newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //TODO - modify notification based on current sit - TO GET EXTRA
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "ceva")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("titlu")
+                .setContentText("context")
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1234, mBuilder.build());
+    }
+}
