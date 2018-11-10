0206b43d8e3432563e140cb4c572e91137bf7d36
atotputerNICA <nicadanamaria@gmail.com>
2018-11-10 10:30:38 +0200

    #FantasticBugsAndWhereToFindThem

--- Android/JuliasHouse/app/src/main/java/com/example/pemil/juliashouse/MainActivity.java
+++ Android/JuliasHouse/app/src/main/java/com/example/pemil/juliashouse/MainActivity.java
@@ -5,18 +5,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     //Declare a private RequestQueue variable
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private static MainActivity mInstance;
 
     private Context context;
+    private int id;
 
 
     public static synchronized MainActivity getInstance() {
@@ -63,11 +71,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mInstance = this;
         Intent intent = getIntent();
-        int id = intent.getIntExtra("id", -1);
+        id = intent.getIntExtra("id", -1);
         Log.i("ID",id + " ");
-
+        context = this;
+        getSits();
         RecyclerView recyclerView = findViewById(R.id.recycler_View);
         MainAdapter adapter = new MainAdapter(this, getSit());
         recyclerView.setAdapter(adapter);
@@ -82,12 +91,39 @@ public class MainActivity extends AppCompatActivity {
     public static ArrayList<SingleVertical> getVerticalData() {
         ArrayList<SingleVertical> singleVerticals = new ArrayList<>();
         singleVerticals.add(new SingleVertical("Sit 1", "10/11/2018", "10/11/2018", R.drawable.task3));
-        singleVerticals.add(new SingleVertical("Sit 2", "10/11/2018", "10/11/2018", R.drawable.task4));
+        singleVerticals.add(new SingleVertical("Sit 2","10/11/2018", "10/11/2018" , R.drawable.task4));
         singleVerticals.add(new SingleVertical("Sit 3", "10/11/2018", "10/11/2018", R.drawable.task));
 
         return singleVerticals;
     }
 
+    public void getSits() {
+        String url = "https://code-for-good.herokuapp.com/api/user/" + id + "/getAll";
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("Sits", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Failure Callback
+                        Log.i("Error", error.toString());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/json");
+                return params;
+            }
+        };
+        // Adding the request to the queue along with a unique string tag
+        MainActivity.getInstance().addToRequestQueue(jsonObjReq, "getRequest");
+    }
     public void openSit(View view) {
         Intent intent = new Intent(this, SitActivity.class);
         startActivity(intent);
