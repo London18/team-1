0236770cd750591619d5da24119bb2660aab9398
pemil <p.emilia13@yahoo.com>
2018-11-10 04:25:14 +0200

    added broadcast receiver for notifications

---
+++ Android/JuliasHouse/app/src/main/java/com/example/pemil/juliashouse/notifications/NotificationReceiver.java
@@ -0,0 +1,16 @@
+package com.example.pemil.juliashouse.notifications;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    public NotificationReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent intent1 = new Intent(context, NotificationIntentService.class);
+        context.startService(intent1);
+    }
+}
